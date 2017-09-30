1294a06ca40d4e51102d6270ff1d9f2b1b6666ca
pmx003theo <worldpeace871219>
2017-09-30 14:45:55 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	int c;
+	c=in.nextInt();
+	System.out.printf("%d",c*9/5+32);
+
+
+    }
+}
